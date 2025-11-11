2eb4a06d8794e26fd58f1f25bcd3682300245516
kero <kerolesadel297@gmail.com>
2025-11-11 19:32:07 +0200

    download material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./page/home/Home";
 import About from "./page/about/About";
-
+import NavBar from "./navBar/navbar";
 export default function App() {
   return (
     <div className="p-6">
-      <nav className="mb-4">
-        <Link to="/" className="bg-dark-blue mr-4">Home</Link>
-        <Link to="/about" className="bg-secondary">About</Link>
-      </nav>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
