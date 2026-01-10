691f2b6f851e9057b87643a7f50697faf2885131
alshaimaahassan <dev.shaimaahassan@gmail.com>
2026-01-10 19:47:37 +0200

    fix console.log

--- src/page/favorite/favorite.tsx
+++ src/page/favorite/favorite.tsx
@@ -57,7 +57,7 @@ const handleRemoveFromFavorites = (id: number) => {
       ) : (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
          {tours.map((tour,index:number) => {
-  console.log(tour);
+
   return (
             <Card
               key={tour.id}
